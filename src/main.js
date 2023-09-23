b393cc99931306765ef893a40582ac3b7a127ce9
clovermini22 <clovermini22@gmail.com>
2023-09-24 02:04:25 +0900

    fix: particle load error

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 import './assets/uikit.css'
 import './assets/uikit.js'
 import './assets/uikit-icons.js'
-import './assets/particles.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
