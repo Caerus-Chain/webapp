fe88969290b027dfdbaf9044a17b82c4507d46df
lukepark327 <lukepark327@gmail.com>
2023-09-24 02:52:31 +0900

    feat: code highlight

--- src/components/CodeView.vue
+++ src/components/CodeView.vue
@@ -5,13 +5,11 @@
     <div class="uk-child-width-1-2@m uk-grid-match" uk-grid>
       <div class="left">
         <div uk-scrollspy="cls: uk-animation-slide-left; repeat: false">
-          <pre><code>
-        caerus(
-            address(this),    // account
-            0,                // slot number
-            blockNumber       // block number
-        );
-      </code></pre>
+          <highlightjs language='solidity' code="bytes32 result = caerus(
+  address(this),        // account
+  slotNumber,           // slot number
+  blockNumber           // block number
+);" />
         </div>
       </div>
       <div class="right">
@@ -21,6 +19,32 @@
             dolor sit amet, consectetur adipiscing elit.<br />Lorem ipsum dolor sit amet,
             consectetur adipiscing elit.
           </p>
+        </div>
+      </div>
+    </div>
+    <div class="uk-child-width-1-2@m uk-grid-match" uk-grid>
+      <div class="left">
+        <div uk-scrollspy="cls: uk-animation-slide-left; repeat: false">
+          <highlightjs language='solidity' code="bytes memory args = abi.encodePacked(
+  address(this),        // account
+  slotNumber,           // slot number
+  blockNumber           // block number
+);
+
+(
+  bool success,         // status
+  bytes memory result   // result (bytes)
+) = address(0x13).staticcall(args);" />
+        </div>
+      </div>
+      <div class="right">
+        <div uk-scrollspy="cls: uk-animation-slide-right; repeat: false">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit.<br />Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </p>
+
         </div>
       </div>
     </div>
@@ -34,9 +58,11 @@
   background-color: rgb(0, 0, 0);
   text-align: center;
 }
+
 .right {
   text-align: left;
 }
+
 .title {
   text-align: center;
   border: 3px solid rgba(255, 255, 255, 0.505);
@@ -44,6 +70,7 @@
   border-radius: 50px 50px 50px 50px;
   margin-bottom: 70px;
 }
+
 .title h1 {
   position: relative;
   top: 10px;
@@ -53,16 +80,19 @@
   font-weight: 500;
   color: rgba(255, 255, 255, 0.89);
 }
+
 h1 {
   font-size: 3rem;
   font-weight: 800;
   color: rgba(255, 255, 255, 0.89);
   padding-bottom: 50px;
 }
+
 p {
   color: rgba(255, 255, 255, 0.89);
   font-size: 2rem;
 }
+
 pre {
   text-align: left;
   font-size: 1.5rem;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import './assets/uikit.css'
 import './assets/uikit.js'
 import './assets/uikit-icons.js'
 
+import 'highlight.js/styles/stackoverflow-light.css'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+hljs.registerLanguage('javascript', javascript);
+
+const app = createApp(App)
+app.use(hljsVuePlugin)
+app.mount('#app')
